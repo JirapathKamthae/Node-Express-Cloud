e7c5d79bf5b6c4a102d5577d0eb99b67a7ba2399
JirapathKamthae <6206021621023@fitm.kmutnb.ac.th>
2021-02-11 11:19:44 +0700

    Go2!!

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT;
 
-app.get("/",(req, res)=>{
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(port, () =>{
-console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
